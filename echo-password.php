43ac48132ca8fe82d83fc1c04e7ec9370831d67d
Toby <66125158+28toby10@users.noreply.github.com>
2022-04-11 14:03:37 +0200

    v

---
+++ echo-password.php
@@ -0,0 +1,77 @@
+<?php
+// Define variables and initialize with empty values
+$password = $confirm_password = "";
+$password_err = $confirm_password_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    
+    // Validate password
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Voer een wachtwoord in.";     
+    } elseif(strlen(trim($_POST["password"])) < 6){
+        $password_err = "Wachtwoord moet minimaal 6 tekens bevatten.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate confirm password
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Bevestig het wachtwoord.";     
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && ($password != $confirm_password)){
+            $confirm_password_err = "Wachtwoord komt niet overeen.";
+        }
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($password_err) && empty($confirm_password_err)){
+        
+        // Set parameters
+        $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash   
+    }
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Idk</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 360px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Idk</h2>
+        <p>Ja iets.</p>
+
+        <?php 
+        if(!empty($param_password)){
+            echo '<div class="alert alert-danger">' . $param_password . '</div>';
+        }        
+        ?>
+
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">   
+            <div class="form-group">
+                <label>Wachtwoord</label>
+                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <label>Bevestig Wachtwoord</label>
+                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
+                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Creër">
+            </div>
+            <p>Heb je al een account? <a href="login.php">Log in hier</a>.</p>
+        </form>
+    </div>    
+</body>
+</html>
